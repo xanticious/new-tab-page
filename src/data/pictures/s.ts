655b56842dd6028c79e44d1ce37a3e2d45434c03
xanticious <TonyLitchfield@Hotmail.com>
2025-07-17 22:19:02 -0600

    Fix for one preloaded picture file

---
+++ src/data/pictures/s.ts
@@ -0,0 +1,13 @@
+import { Picture } from '@/types';
+
+// Pictures starting with 'S'
+export const picturesS: Picture[] = [
+  {
+    id: 'pic_stackoverflow',
+    name: 'Stack Overflow',
+    base64ImageData:
+      'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAABx0lEQVRYhe2Xr0rEQBjFz2VhcVhYXBYWl4XFZWFhcVkWFpeHhYXFZWFxWVgcFhaXhcVlYXFZFhaXhYXFZWFhcVlYXBYWFpeF3z/9AwjkgTr/2/5A8fTsAUGg6IEO84AAA+IAA/6AAQ2vAwzAAA/DAAyEgQEY4GGOKwsF/wANBAQFBeQgkNI2i4QH8ADgOaB5AIwOB6/AFUByh8KLCOo=',
+    altText: 'Stack Overflow programming Q&A website logo',
+    readonly: true,
+  },
+];
